9d873e43cf69607825487cf1f8fc9f7246910923
Yuqing Xie <yuqingxie@autosys-16.w3qkn5pw12uu3kate4hkieqkpa.bx.internal.cloudapp.net>
2021-10-31 08:42:59 +0000

    2scale

--- test_get_res.py
+++ test_get_res.py
@@ -1,23 +1,23 @@
 import csv
 import re
 
-print("pred")
-for train_size in [10,25,50,100,150,200,300,450,600]:
-    print(train_size)
-    for i in range(5):
-        with open('test_1028/DNN_2checkout_'+str(train_size)+"_"+str(i)) as f:
-            text = f.read()
-            sentence = text.split()
-            print(sentence[-1])
-    print()
-
-# print("real")
-# for train_size in [10,25,50,100,150,200,300,450,600]:
+# print("pred")
+# for train_size in [10,25,50,100,150,200,300,400]:
 #     print(train_size)
 #     for i in range(5):
-#         with open('test_res/datares_fluxion_'+str(train_size)+"_"+str(i)+".csv") as csvfile:
-#             reader = csv.DictReader(csvfile)
-#             for row in reader:
-#                 if row["service"] == "frontend":
-#                     print(row["0.90"])
+#         with open('test_2scale/DNN_2scale_'+str(train_size)+"_"+str(i)) as f:
+#             text = f.read()
+#             sentence = text.split()
+#             print(sentence[-1])
 #     print()
+
+print("real")
+for train_size in [10,25,50,100,150,200,300,400]:
+    print(train_size)
+    for i in range(5):
+        with open('test_res/dataDNN_2scale_'+str(train_size)+"_"+str(i)+".csv") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if row["service"] == "frontend":
+                    print(row["0.90"])
+    print()
